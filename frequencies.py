6bb803aa3496a3d57b5ad631a124ae93a5af2ce6
Shrestha, Saujan (k21075139) <saujan.shrestha@kcl.ac.uk>
2022-09-27 18:28:23 +0100

    Add frequency counter

--- frequencies.py
+++ frequencies.py
@@ -6,8 +6,12 @@ def frequencies(items):
     for item in items:
         item = str(item)
         if item in frequencies:
-            frequencies[item] = 1
+            frequencies[item] = frequencies[item] + 1
         else:
-            frequencies[item] += 1
+            frequencies[item] = 1
     
     return frequencies
+
+items = [2,5,6,"2"]
+
+print(frequencies(items))
